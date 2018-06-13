19d0ce32048bf3a9015cf1b1ee4c96806d9e9a77
Jizbel Abel Johnson <35666632+HawkJizbel@users.noreply.github.com>
2018-06-13 21:17:21 +0100

    Transpose Result

--- PNM/src/Prime.java
+++ PNM/src/Prime.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Prime {
 
@@ -9,35 +10,39 @@ public class Prime {
 		prime[0][0] = 2;
 		prime[0][1] = 3;
 		prime[0][2] = 5;
-		prime[1][0] = 0;
-		prime[1][1] = 0;
-		prime[1][2] = 0;
-		prime[2][0] = 0;
-		prime[2][1] = 0;
-		prime[2][2] = 0;
-		
-		for (int i = 0; i < prime.length; i++) 
-        {
-            for (int j = 0; j < prime[i].length; j++) 
-            {
-            	invprime[j][i] = prime[i][j];
-                 System.out.print(prime[i][j] + " ");
-            }
-            System.out.print("\n");
-        }
-		
-		if (prime.length > 0) {
-		for (int i = 0; i < invprime.length; i++) 
-        {
-            for (int j = 0; j < invprime[i].length; j++) 
-            { 
-            	System.out.print(invprime[i][j] + " ");
-            }
-            System.out.print("\n");
-        }
-		
+
+		for (int i = 0; i < prime.length; i++) {
+			for (int j = 0; j < prime[i].length; j++) {
+				invprime[j][i] = prime[i][j];
+				System.out.print(prime[i][j] + " ");
+			}
+			System.out.print("\n");
 		}
 
-	}
+		if (prime.length > 0) {
+			for (int i = 0; i < invprime.length; i++) {
+				for (int j = 0; j < invprime[i].length; j++) {
+					System.out.print(invprime[i][j] + " ");
+				}
+				System.out.print("\n");
+			}
+			System.out.print("\n");
 
+		}
+
+		if (prime.length > 0) {
+			for (int i = 0; i < finalprime.length; i++) {
+				for (int j = 0; j < finalprime[i].length; j++) {
+					for (int k = 0; k < finalprime.length; k++) {
+						for (int l = 0; l < finalprime.length; l++) {
+							finalprime[i][j] = prime[k][l] * invprime[j][i];
+							System.out.print(finalprime[i][j] + " ");
+						}
+					}
+					System.out.print("\n");
+				}
+			}
+			//System.out.print(Arrays.deepToString(finalprime));
+		}
+	}
 }
