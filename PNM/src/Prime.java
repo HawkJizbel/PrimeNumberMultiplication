37f10cef602a837578bf54300c57cb6ea57f65ae
Jizbel Abel Johnson <35666632+HawkJizbel@users.noreply.github.com>
2018-06-19 13:49:24 +0100

    Final Code Rev 1

--- PNM/src/Prime.java
+++ PNM/src/Prime.java
@@ -1,48 +1,85 @@
+/*
+ * Code created by Jizbel Abel Johnson
+ * The code calculates the prime numbers and then outputs prime multiplication table
+ */
+
 import java.util.Scanner;
 
 public class Prime {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		
+
+		//Prime Number User Input
 		Scanner inp = new Scanner(System.in);
-		System.out.println("Enter the prime number");
+		System.out.println("Enter the number:");
 		int pnum = inp.nextInt();
-		
-		int[][] prime = new int[pnum][pnum];
+		inp.close();
+
+		//Initialise Array
+		int[][] iniprime = new int[pnum][pnum];
 		int[][] invprime = new int[pnum][pnum];
 		int[][] finalprime = new int[pnum][pnum];
+
+		int n = pnum * 10;
+		int a = 0;
+
+		int[][] tempprime = new int[n][n];
+
+		//Prime Number Calculation
+		boolean prime[] = new boolean[n + 1];
+		for (int i = 0; i < n; i++)
+			prime[i] = true;
+		for (int p = 2; p * p <= n; p++) {
+
+			if (prime[p] == true) {
+
+				for (int i = p * 2; i <= n; i += p)
+					prime[i] = false;
+			}
+		}
+		//Copy Prime Number to Array
+		for (int i = 2; i <= n; i++) {
+
+			if (prime[i] == true) {
+				tempprime[0][a] = i;
+				a++;
+
+			}
+
+		}
+
+		//Resize and Copy Prime Numbers to Initial Array
+		for (int i1 = 0; i1 < pnum; i1++) {
+			for (int j1 = 0; j1 < pnum; j1++) {
+				iniprime[i1][j1] = tempprime[i1][j1];
+			}
+		}
+
+		//Clear Temporary Array
+		tempprime = new int[0][0];
+
+		System.out.print("The Prime Numbers are: ");
 		
-		prime[0][0] = 2;
-		prime[0][1] = 3;
-		prime[0][2] = 5;
-
-		for (int i = 0; i < prime.length; i++) {
-			for (int j = 0; j < prime[i].length; j++) {
-				invprime[j][i] = prime[i][j];
-				System.out.print(prime[i][j] + " ");
+		//Transpose Prime Number Array
+		for (int i = 0; i < iniprime.length; i++) {
+			for (int j = 0; j < iniprime[i].length; j++) {
+				invprime[j][i] = iniprime[i][j];
+				System.out.print(iniprime[0][j] + " ");
 			}
 			System.out.print("\n");
-		}
-
-		if (prime.length > 0) {
-			for (int i = 0; i < invprime.length; i++) {
-				for (int j = 0; j < invprime[i].length; j++) {
-					System.out.print(invprime[i][j] + " ");
-				}
-				System.out.print("\n");
-			}
 			System.out.print("\n");
-
+			break;
 		}
 
-		if (prime.length > 0) {
+
+		if (iniprime.length > 0) {
 			for (int i = 0; i < finalprime.length; i++) {
 				for (int j = 0; j < finalprime[i].length; j++) {
 					for (int k = 0; k < finalprime.length; k++) {
 						for (int l = 0; l < finalprime.length; l++) {
-							finalprime[k][l] = prime[k][l] * invprime[j][i];
-							System.out.print(finalprime[k][l] + " ");
+							finalprime[k][l] = iniprime[k][l] * invprime[j][i];
+							System.out.print(finalprime[k][l] + " | ");
 						}
 						break;
 					}
@@ -51,6 +88,5 @@ public class Prime {
 				break;
 			}
 		}
-		inp.close();
 	}
 }
